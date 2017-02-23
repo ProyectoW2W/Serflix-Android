4351d33bff33177fffe86ea104af7a01b16d87e6
aran-fern <aranda.perez.fernando@gmail.com>
2017-02-23 17:50:04 +0100

    Updates MovieCard view

--- app/src/main/java/com/example/dam/serflix/Controller/MovieCard.java
+++ app/src/main/java/com/example/dam/serflix/Controller/MovieCard.java
@@ -1,25 +1,69 @@
 package com.example.dam.serflix.Controller;
 
 import android.content.Context;
-import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.example.dam.serflix.Model.Movie;
 import com.example.dam.serflix.R;
 
-public class MovieCard extends CardView{
+
+public class MovieCard extends LinearLayout {
+    TextView tags;
+    TextView title;
+    TextView year;
+    TextView cast;
+    TextView description;
+    ImageView image;
+
+
     public MovieCard(Context context) {
         super(context);
-        inflate(getContext(), R.layout.movie_card, this);
+        initializeView();
     }
 
     public MovieCard(Context context, AttributeSet attrs) {
         super(context, attrs);
-        inflate(getContext(), R.layout.movie_card, this);
+        initializeView();
 
     }
 
     public MovieCard(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initializeView();
+    }
+
+    public void initializeView() {
         inflate(getContext(), R.layout.movie_card, this);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        tags = (TextView) findViewById(R.id.tags);
+        title = (TextView) findViewById(R.id.title);
+        year = (TextView) findViewById(R.id.year);
+        cast = (TextView) findViewById(R.id.cast);
+        description = (TextView) findViewById(R.id.description);
+        image = (ImageView) findViewById(R.id.poster);
+        super.onFinishInflate();
+
+        Movie m = new Movie();
+        m.setCast("Jennifer Lawrence, Chris Pratt, Morten Tyldum, Jon Spaihts");
+        m.setDescription("A spacecraft traveling to a distant colony planet and transporting thousands of people has a malfunction in its sleep chambers. As a result, two passengers are awakened 90 years early.3");
+        m.setTags("Adventure, Drama, Romance, Science Fiction, isolation, spaceship, space");
+        m.setTitle("Passengers");
+        m.setYear("2016");
+        setMovieCard(m);
+    }
+
+    public void setMovieCard(Movie m) {
+        //image.setImageResource();
+        title.setText(m.getTitle());
+        year.setText(m.getYear());
+        description.setText(m.getDescription());
+        cast.setText(m.getCast());
+        tags.setText(m.getTags());
     }
 }
---
+++ app/src/main/java/com/example/dam/serflix/Model/Movie.java
@@ -0,0 +1,69 @@
+package com.example.dam.serflix.Model;
+
+public class Movie {
+    private String title;
+    private String cast;
+    private String tags;
+    private String poster;
+    private String description;
+    private String year;
+
+    public Movie(String title, String cast, String tags, String poster, String description, String year) {
+        this.title = title;
+        this.cast = cast;
+        this.tags = tags;
+        this.poster = poster;
+        this.description = description;
+        this.year = year;
+    }
+
+    public Movie() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getYear() {
+        return year;
+    }
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getCast() {
+        return cast;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+}
