40e35e24503fabbcb14bc6ed19b670ed1c03257c
DAM <victoramadorsanjuan@gmail.com>
2017-03-01 19:38:10 +0100

    Obtain Lat and Lon #7 (revisar)

--- app/src/main/java/com/example/dam/serflix/Controller/Activities/RequestActivity.java
+++ app/src/main/java/com/example/dam/serflix/Controller/Activities/RequestActivity.java
@@ -1,12 +1,22 @@
 package com.example.dam.serflix.Controller.Activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.dam.serflix.Controller.DatePickerView;
 import com.example.dam.serflix.Controller.TimePickerView;
@@ -14,8 +24,12 @@ import com.example.dam.serflix.R;
 
 import java.util.Date;
 
-public class RequestActivity extends AppCompatActivity{
+public class RequestActivity extends AppCompatActivity {
     private Button sendRequestButton;
+    private Button LocationButton;
+    private TextView CoordinatesText;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     Spinner companySpinner;
     TimePickerView timePickerView;
@@ -32,6 +46,10 @@ public class RequestActivity extends AppCompatActivity{
         companySpinner = (Spinner) findViewById(R.id.companySpinner);
         sendRequestButton = (Button) findViewById(R.id.sendRequestButton);
 
+        //botones lat lon
+        LocationButton = (Button) findViewById(R.id.buttonLatLon);
+        CoordinatesText = (TextView) findViewById(R.id.textLatLon);
+
         //Establecer valores Company Spinner
         Spinner spinner = (Spinner) findViewById(R.id.companySpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -39,13 +57,18 @@ public class RequestActivity extends AppCompatActivity{
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        //LLAMAR A GPS
+        ObtainLatLon();
+
+
+
         //Listeners
         sendRequestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println("Company: "+companySpinner.getSelectedItem());
-                System.out.println("Day: "+datePickerView.getDate());
-                System.out.println("Time: "+timePickerView.getTime());
+                System.out.println("Company: " + companySpinner.getSelectedItem());
+                System.out.println("Day: " + datePickerView.getDate());
+                System.out.println("Time: " + timePickerView.getTime());
 
                 //Enviar request
 
@@ -55,4 +78,65 @@ public class RequestActivity extends AppCompatActivity{
             }
         });
     }
+
+    private void ObtainLatLon() {
+        locationManager = (LocationManager) getSystemService((LOCATION_SERVICE));
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                CoordinatesText.append("\n "+location.getLatitude()+" "+location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.INTERNET
+                }, 10);
+            }
+            return;
+        }else{
+            configureButton();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configureButton();
+                    return;
+        }
+
+    }
+
+    private void configureButton(){
+        LocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+            }
+        });
+    }
+
+
+
+
+
 }
