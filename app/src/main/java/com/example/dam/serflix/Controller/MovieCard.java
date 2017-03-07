5a372e74ca78e5deca75a7a5829419ef11e98d06
aran-fern <aranda.perez.fernando@gmail.com>
2017-03-07 19:08:19 +0100

    Adds horizontal swipe recognition

--- app/src/main/java/com/example/dam/serflix/Controller/MovieCard.java
+++ app/src/main/java/com/example/dam/serflix/Controller/MovieCard.java
@@ -2,9 +2,11 @@ package com.example.dam.serflix.Controller;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.dam.serflix.Model.Movie;
 import com.example.dam.serflix.R;
@@ -18,6 +20,9 @@ public class MovieCard extends LinearLayout {
     TextView cast;
     TextView description;
     ImageView image;
+    //Swipe
+    private float x1,x2;
+    static final int MIN_DISTANCE = 150;
 
 
     public MovieCard(Context context) {
@@ -58,5 +63,46 @@ public class MovieCard extends LinearLayout {
         description.setText(m.getDescription());
         cast.setText(m.getCast());
         tags.setText(m.getTags());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                System.out.println("TOUCHEVENT1");
+                x1 = event.getX();
+                //break;
+                return true;
+            case MotionEvent.ACTION_UP:
+                System.out.println("TOUCHEVENT2");
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+
+                if (Math.abs(deltaX) > MIN_DISTANCE)
+                {
+                    // Left to Right swipe action
+                    if (x2 > x1)
+                    {
+                        Toast.makeText(this.getContext(), "Pelicula aceptada", Toast.LENGTH_SHORT).show ();
+                    }
+
+                    // Right to left swipe action
+                    else
+                    {
+                        Toast.makeText(this.getContext(), "Pelicula rechazada", Toast.LENGTH_SHORT).show ();
+                    }
+
+                }
+                else
+                {
+                    // consider as something else - a screen tap for example
+                }
+                break;
+            default:
+                System.out.println("TOUCHEVENT3");
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 }
