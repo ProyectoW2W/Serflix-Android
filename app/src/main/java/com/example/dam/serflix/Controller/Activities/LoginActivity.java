f01d5266408534e9dc13ea18247668f764e9a0ae
DAM <sergiodiaz808@gmail.com>
2017-05-25 21:09:16 +0200

    Location modified.

--- app/src/main/java/com/example/dam/serflix/Controller/Activities/LoginActivity.java
+++ app/src/main/java/com/example/dam/serflix/Controller/Activities/LoginActivity.java
@@ -48,14 +48,53 @@ public class LoginActivity extends AppCompatActivity implements LoginCallback {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
-        username = (EditText)findViewById(R.id.editEmail);
-        pass = (EditText)findViewById(R.id.editPass);
+        username = (EditText) findViewById(R.id.editEmail);
+        pass = (EditText) findViewById(R.id.editPass);
 
         //Obtenemos latitud y longitud al iniciar la aplicacion para poderla obtener mas rapidamente y poder pasarla correctamente a RequestActivity!!!!!!!
         //HAY QUE LLAMAR DOS VECES A ObtainLatLon PARA QUE RELLENE CON LATLON Y NO PETE EL LOCATION MANAGER
-        ObtainLatLon();
-        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
-        ObtainLatLon();
+        locationManager = (LocationManager) getSystemService((LOCATION_SERVICE));
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(android.location.Location location) {
+                Log.d("Coordenadas", "Latitud = " + location.getLatitude() + " , Longitud = " + location.getLongitude());
+                latlon = String.valueOf(location.getLatitude() + "," + location.getLongitude());
+                System.out.println(latlon + " --------- latlon1");
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                boolean gps_enabled = false;
+                boolean network_enabled = false;
+
+                try {
+                    gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+                } catch(Exception ex) {}
+
+                try {
+                    network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+                } catch(Exception ex) {}
+
+                if(!gps_enabled && !network_enabled) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
 
         Button login_btn = (Button) findViewById(R.id.login_btn);
         login_btn.setOnClickListener(new View.OnClickListener(){
@@ -106,47 +145,6 @@ public class LoginActivity extends AppCompatActivity implements LoginCallback {
     public void onFailure(Throwable t) {
         Log.e("LoginActivity->", "performLogin->onFailure ERROR " + t.getMessage());
         Toast.makeText(this, "Credenciales incorrectos", Toast.LENGTH_LONG).show();
-    }
-
-
-
-    private void ObtainLatLon() {
-
-        locationManager = (LocationManager) getSystemService((LOCATION_SERVICE));
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(android.location.Location location) {
-                Log.d("Coordenadas", "Latitud = " +location.getLatitude() + " , Longitud = " + location.getLongitude());
-                latlon = String.valueOf(location.getLatitude()+","+location.getLongitude());
-                System.out.println(latlon + " --------- latlon1");
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        };
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{
-                        Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.INTERNET
-                }, 10);
-            }
-            return;
-        }else{
-            //configureButton();
-        }
     }
 
     @Override
