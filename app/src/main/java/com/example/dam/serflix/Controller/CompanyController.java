322e8a5daa8a353c6f0b70717d72baf9de3ceb7a
albertocomu <albertocomu@gmail.com>
2017-05-31 17:47:45 +0200

    company controller

---
+++ app/src/main/java/com/example/dam/serflix/Controller/CompanyController.java
@@ -0,0 +1,29 @@
+package com.example.dam.serflix.Controller;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.example.dam.serflix.R;
+
+/**
+ * Created by albertocomu on 29/5/17.
+ */
+
+public class CompanyController extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.companySpinner)
+                .setItems(R.array.company_array, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // The 'which' argument contains the index position
+                        // of the selected item
+                    }
+                });
+        return builder.create();
+    }
+}
