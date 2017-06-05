462e26ad12a648aa8ad2d3a37ddaa16fb306881c
sergiodiaz <sergiodiaz808@gmail.com>
2017-06-05 20:08:00 +0200

    Repairs crash when app starts.

--- app/src/main/java/com/example/dam/serflix/Controller/Activities/LoginActivity.java
+++ app/src/main/java/com/example/dam/serflix/Controller/Activities/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.dam.serflix.Controller.Activities;
 
 import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationListener;
@@ -12,6 +14,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -51,6 +54,9 @@ public class LoginActivity extends AppCompatActivity implements LoginCallback,
     protected GoogleApiClient mGoogleApiClient;
     protected android.location.Location mLastLocation;
     String latlon = "";
+    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 101;
+    private static final int MY_PERMISSIONS_REQUEST_COARSE_LOCATION = 102;
+    private boolean permissionIsGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,51 +65,59 @@ public class LoginActivity extends AppCompatActivity implements LoginCallback,
         setContentView(R.layout.activity_login);
         username = (EditText) findViewById(R.id.editEmail);
         pass = (EditText) findViewById(R.id.editPass);
+        statusCheck();
 
-        locationManager = (LocationManager) getSystemService((LOCATION_SERVICE));
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(android.location.Location location) {
-                Log.d("Coordenadas", "Latitud = " + location.getLatitude() + " , Longitud = " + location.getLongitude());
-                latlon = String.valueOf(location.getLatitude() + "," + location.getLongitude());
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-                boolean gps_enabled = false;
-                boolean network_enabled = false;
-
-                try {
-                    gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-                } catch (Exception ex) {
-                }
-
-                try {
-                    network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-                } catch (Exception ex) {
-                }
-
-                if (!gps_enabled && !network_enabled) {
-                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                    startActivity(intent);
-                }
-            }
-        };
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
 
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+            }else{
+                permissionIsGranted = true;
+            }
+        }
+        if (permissionIsGranted){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 0, locationListener);
+        }
+
+            locationManager = (LocationManager) getSystemService((LOCATION_SERVICE));
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(android.location.Location location) {
+                    Log.d("Coordenadas", "Latitud = " + location.getLatitude() + " , Longitud = " + location.getLongitude());
+                    latlon = String.valueOf(location.getLatitude() + "," + location.getLongitude());
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+                    boolean gps_enabled = false;
+                    boolean network_enabled = false;
+
+                    try {
+                        gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+                    } catch (Exception ex) {
+                    }
+
+                    try {
+                        network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+                    } catch (Exception ex) {
+                    }
+
+                    if (!gps_enabled && !network_enabled) {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
+                    }
+                }
+            };
 
         Button login_btn = (Button) findViewById(R.id.login_btn);
         TextView sign_btn = (TextView) findViewById(R.id.signUp_btn);
@@ -174,7 +188,7 @@ public class LoginActivity extends AppCompatActivity implements LoginCallback,
                 final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
                 try {
                     startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-                } catch (android.content.ActivityNotFoundException anfe) {
+                } catch (ActivityNotFoundException anfe) {
                     startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
                 }
             }
@@ -185,6 +199,34 @@ public class LoginActivity extends AppCompatActivity implements LoginCallback,
             buildGoogleApiClient();
         }
     }
+
+    public void statusCheck() {
+        locationManager = (LocationManager) getSystemService(getApplicationContext().LOCATION_SERVICE);
+
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+
+        }
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled. Do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
 
     private void attemptLogin() {
         String usu = username.getText().toString();
@@ -208,14 +250,28 @@ public class LoginActivity extends AppCompatActivity implements LoginCallback,
     @Override
     protected void onStart() {
         super.onStart();
-        mGoogleApiClient.connect();
+        if (permissionIsGranted){
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (permissionIsGranted) {
+            if (mGoogleApiClient.isConnected()) {
+                mGoogleApiClient.disconnect();
+            }
+        }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
+        if (permissionIsGranted) {
+            if (mGoogleApiClient.isConnected()) {
+                mGoogleApiClient.disconnect();
+            }
         }
     }
 
@@ -239,13 +295,14 @@ public class LoginActivity extends AppCompatActivity implements LoginCallback,
     @Override
     public void onConnectionSuspended(int cause) {
         Log.i("LoginActivity", "Connection suspended");
-        mGoogleApiClient.connect();
+        if (permissionIsGranted) {
+            mGoogleApiClient.connect();
+        }
     }
 
     @Override
     public void onSuccess(UserToken userToken) {
         Intent intent = new Intent(LoginActivity.this, RequestActivity.class);
-        Toast.makeText(this, "Correcto! Iniciando sesion", Toast.LENGTH_LONG).show();
         intent.putExtra("latlon", latlon);
         startActivity(intent);
         finish();
@@ -255,15 +312,32 @@ public class LoginActivity extends AppCompatActivity implements LoginCallback,
     @Override
     public void onFailure(Throwable t) {
         Log.e("LoginActivity->", "performLogin->onFailure ERROR " + t.getMessage());
-        Toast.makeText(this, "Incorrect User.", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Incorrect User", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    permissionIsGranted = true;
+                }else {
+                    permissionIsGranted = false;
+                    Toast.makeText(getApplicationContext(), "This app requires location permission to be granted", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case MY_PERMISSIONS_REQUEST_COARSE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    permissionIsGranted = true;
+                }else {
+                    permissionIsGranted = false;
+                    Toast.makeText(getApplicationContext(), "This app requires location permission to be granted", Toast.LENGTH_SHORT).show();
+                }
+                break;
             case 10:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    return;
+                    permissionIsGranted = true;
+                break;
         }
     }
 }
